5fbcd63d7c692b5196da8c73b35e393441609a3d
Mos3ab696 <mos3ab696@gmail.com>
2024-06-16 23:50:18 +0300

    task

--- markdown2html.py
+++ markdown2html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-A script that codes markdown to HTML
+A script that converts markdown to HTML
 '''
 import sys
 import os
@@ -23,6 +23,7 @@ if __name__ == '__main__':
         print(f'Missing {input_file}', file=sys.stderr)
         sys.exit(1)
 
+    ol_opened = False  # Flag to track if we've started an ordered list
     ul_opened = False  # Flag to track if we've started an unordered list
     with open(input_file, encoding='utf-8') as file_1:
         html_content = []
@@ -37,11 +38,18 @@ if __name__ == '__main__':
                 html_content.append(
                     f'<h{h_level}>{heading[1]}</h{h_level}>\n'
                 )
-                ul_opened = False  # Reset flag when exiting a heading
+                ol_opened = ul_opened = False  # Reset flags when exiting a heading
+            elif line.strip().startswith(('1.', '2.', '3.')):  # Check for ordered list items
+                if not ol_opened:
+                    html_content.append('<ol>\n')  # Start ordered list
+                    ol_opened = True
+                    ul_opened = False  # Ensure no unordered list is opened
+                html_content.append(f'<li>{line[2:]}</li>\n')
             elif line.startswith('- '):  # Check for unordered list items
                 if not ul_opened:
                     html_content.append('<ul>\n')  # Start unordered list
                     ul_opened = True
+                    ol_opened = False  # Ensure no ordered list is opened
                 html_content.append(f'<li>{line[2:]}</li>\n')
             else:
                 html_content.append(line)
@@ -49,6 +57,10 @@ if __name__ == '__main__':
                     # End unordered list if necessary
                     html_content.append('</ul>\n')
                     ul_opened = False
+                if ol_opened:
+                    # End ordered list if necessary
+                    html_content.append('</ol>\n')
+                    ol_opened = False
 
     with open(output_file, 'w', encoding='utf-8') as file_2:
         file_2.writelines(html_content)
